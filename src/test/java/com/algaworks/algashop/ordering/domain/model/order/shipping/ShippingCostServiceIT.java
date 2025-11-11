acf631890523be6205c33f2d50748d3f455a7b15
queirozg <george.ba.fsa@gmail.com>
2025-11-11 10:33:01 -0300

    [Test] added wiremock ShippingCostIT

--- src/test/java/com/algaworks/algashop/ordering/domain/model/order/shipping/ShippingCostServiceIT.java
+++ src/test/java/com/algaworks/algashop/ordering/domain/model/order/shipping/ShippingCostServiceIT.java
@@ -1,14 +1,30 @@
 package com.algaworks.algashop.ordering.domain.model.order.shipping;
 
-import com.algaworks.algashop.ordering.domain.model.order.shipping.ShippingCostService.CalculationRequest;
-import com.algaworks.algashop.ordering.domain.model.commons.ZipCode;
+import static com.github.tomakehurst.wiremock.core.WireMockConfiguration.options;
+import static io.restassured.config.JsonConfig.jsonConfig;
+
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.test.annotation.DirtiesContext;
 
-@SpringBootTest
+import com.algaworks.algashop.ordering.domain.model.commons.ZipCode;
+import com.algaworks.algashop.ordering.domain.model.order.shipping.ShippingCostService.CalculationRequest;
+import com.github.tomakehurst.wiremock.WireMockServer;
+
+import io.restassured.RestAssured;
+import io.restassured.path.json.config.JsonPathConfig;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
 class ShippingCostServiceIT {
+
+    @LocalServerPort
+    private int port;
 
     @Autowired
     private ShippingCostService shippingCostService;
@@ -16,8 +32,33 @@ class ShippingCostServiceIT {
     @Autowired
     private OriginAddressService originAddressService;
 
+    private WireMockServer wireMockRapidex;
+
+    @BeforeEach
+    public void setup() {
+        RestAssured.enableLoggingOfRequestAndResponseIfValidationFails();
+        RestAssured.port = port;
+
+        RestAssured.config().jsonConfig(jsonConfig().numberReturnType(JsonPathConfig.NumberReturnType.BIG_DECIMAL));
+
+        wireMockRapidex = new WireMockServer(options()
+                .port(8780)
+                .usingFilesUnderDirectory("src/test/resources/wiremock/rapidex"));
+
+        wireMockRapidex.start();
+
+    }
+
+    @AfterEach
+    public void after() {
+
+        wireMockRapidex.stop();
+
+    }
+
     @Test
     void shouldCalculate() {
+
         ZipCode origin = originAddressService.originAddress().zipCode();
         ZipCode destination = new ZipCode("12345");
 
@@ -26,6 +67,7 @@ class ShippingCostServiceIT {
 
         Assertions.assertThat(calculate.cost()).isNotNull();
         Assertions.assertThat(calculate.expectedDate()).isNotNull();
+
     }
-  
+
 }
