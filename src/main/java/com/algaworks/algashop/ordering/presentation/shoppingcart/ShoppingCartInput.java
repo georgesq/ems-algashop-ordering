92b3d02fef92d7304e5e697ab0851a22fd5dc52a
queirozg <george.ba.fsa@gmail.com>
2025-11-11 10:03:20 -0300

    [Test] added ShoppingCartControllerIT

--- src/main/java/com/algaworks/algashop/ordering/presentation/shoppingcart/ShoppingCartInput.java
+++ src/main/java/com/algaworks/algashop/ordering/presentation/shoppingcart/ShoppingCartInput.java
@@ -1,11 +1,13 @@
 package com.algaworks.algashop.ordering.presentation.shoppingcart;
 
-import jakarta.validation.constraints.NotNull;
-import lombok.Data;
-
 import java.util.UUID;
 
+import jakarta.validation.constraints.NotNull;
+import lombok.Builder;
+import lombok.Data;
+
 @Data
+@Builder
 public class ShoppingCartInput {
 	@NotNull
 	private UUID customerId;
